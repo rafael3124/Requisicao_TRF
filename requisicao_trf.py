81611fa3ae2009ba87c0e81cfbca65d6afc08593
Rafael Araujo <rafael3124@gmail.com>
2023-02-01 22:14:15 -0300

    Alteração no parametro L14 para"a"/loop limite 30x

--- requisicao_trf.py
+++ requisicao_trf.py
@@ -3,15 +3,17 @@ import time
 import json
 
 
-#repetição
-while True:
+# limitando a repetição em 30x
+for x in range (30):
 
     url = 'https://hcomunicaapi.cnj.jus.br/api/v1/comunicacao'
 
     #requisição API
     requisicao = requests.get(url)
     data = requisicao.json()
-    with open("data.json", "w") as outfile:
+    with open("data.json", "a") as outfile:
         json.dump(data, outfile)
-    print('Loop 10s', flush=True)
+    #imprime a numeração da iteração
+    print('Loop 10s Iteração nº: ', x)
+    #tempo de intervalo da iteração 
     time.sleep(10)
